de83e7e40182ce8510814497fd9d47c4da6b1eee
Lyall Stewart <78568904+lyallstewart@users.noreply.github.com>
2021-03-31 18:57:45 +0100

    Update code.py
    Final upload

--- code.py
+++ code.py
@@ -3,13 +3,11 @@ from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras import layers
-
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #import app
 
-batch = 32
+
+batch = 4
 
 generator = ImageDataGenerator(
         rotation_range=40,
@@ -21,29 +19,55 @@ generator = ImageDataGenerator(
         horizontal_flip=True,
         fill_mode='nearest')
 
-train_iterator = generator.flow_from_directory('data/train',batch_size = batch,color_mode='grayscale', class_mode='sparse')
-test_iterator = generator.flow_from_directory('data/test',batch_size = batch,color_mode='grayscale', class_mode='sparse')
+train_iterator = generator.flow_from_directory("C:/Users/Lyall Stewart/Documents/Coding/NeuralNetwork/COVID-19 Classification/data/train",
+                                               batch_size=batch,
+                                               color_mode='grayscale',
+                                               class_mode='sparse')
+test_iterator = generator.flow_from_directory("C:/Users/Lyall Stewart/Documents/Coding/NeuralNetwork/COVID-19 Classification/data/test",
+                                              batch_size=batch,
+                                              color_mode='grayscale',
+                                              class_mode='sparse')
+
 
 def design_model():
-  model = Sequential()
-  model.add(tf.keras.Input(shape=(256, 256, 1)))
+    model = Sequential()
+    model.add(tf.keras.Input(shape=(256, 256, 1)))
 
-  model.add(tf.keras.layers.Conv2D(2, 5, strides=3, activation="relu")) 
-  model.add(tf.keras.layers.MaxPooling2D(pool_size=(5, 5), strides=(5,5)))
-  model.add(tf.keras.layers.Conv2D(4, 3, strides=1, activation="relu")) 
-  model.add(tf.keras.layers.MaxPooling2D(pool_size=(3,2), strides=(2,2)))
+    model.add(tf.keras.layers.Conv2D(2, 5, strides=3, activation="relu"))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(5, 5), strides=(5,5)))
+    model.add(tf.keras.layers.Conv2D(4, 3, strides=1, activation="relu"))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(3,2), strides=(2,2)))
 
-  model.add(tf.keras.layers.Flatten())
-  model.add(tf.keras.layers.Dense(4, activation="softmax"))
+    model.add(tf.keras.layers.Flatten())
+    #model.add(tf.keras.layers.Dense(8, activation="relu"))
+    #model.add(tf.keras.layers.Dropout(.20))
+    model.add(tf.keras.layers.Dense(4, activation='softmax'))
 
-  model.summary()
+    model.summary()
 
-  callback = tf.keras.callbacks.EarlyStopping(monitor='accuracy', patience=3)
+    callback = tf.keras.callbacks.EarlyStopping(monitor='accuracy', patience=5, restore_best_weights=True, verbose=1)
 
-  return model, callback
-  print("Model designed")
+    print("Model designed")
+    return model, callback
 
-model, callback = design_model()
 
-model.compile(loss='sparse_categorical_crossentropy', optimizer=keras.optimizers.Adam(learning_rate=0.01), metrics=['accuracy'])
-model.fit(train_iterator, epochs=50, steps_per_epoch=125, validation_data=test_iterator,validation_steps=100, callbacks=[callback], verbose=1)
+model, es_callback = design_model()
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=keras.optimizers.Adam(learning_rate=0.01),
+              metrics=['accuracy'])
+
+history = model.fit_generator(train_iterator,
+                              epochs=50,
+                              steps_per_epoch=50,
+                              validation_data=test_iterator,
+                              validation_steps=50,
+                              callbacks=[es_callback],
+                              verbose=1)
+
+plt.plot(history.history['accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train'], loc='upper left')
+plt.show()
